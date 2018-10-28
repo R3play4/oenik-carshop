c64d53b08b2fd2e6eadbae73f9e7600fa2389f6e
REPLAY\R3pl4y <nfodor4@gmail.com>
2018-10-28 16:29:29 +0100

    Created CarShop.Program ConsoleApp

--- CSHARP/OENIK_PROG3_2018_2_DVKERF/CarShop.Program/Program.cs
+++ CSHARP/OENIK_PROG3_2018_2_DVKERF/CarShop.Program/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarShop.Data
+namespace CarShop.Program
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
