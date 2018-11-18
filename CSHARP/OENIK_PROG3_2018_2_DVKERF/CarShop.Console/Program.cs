20e939f0934426c44feb37ddb1e84532b82c4ed8
REPLAY\R3pl4y <nfodor4@gmail.com>
2018-11-18 23:35:48 +0100

    Created Main Method in Console.Program

--- CSHARP/OENIK_PROG3_2018_2_DVKERF/CarShop.Console/Program.cs
+++ CSHARP/OENIK_PROG3_2018_2_DVKERF/CarShop.Console/Program.cs
@@ -8,6 +8,7 @@ namespace CarShop.Console
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using MenuItems;
 
     /// <summary>
     /// Main Class. Includes Entry point (Main method)
@@ -20,6 +21,36 @@ namespace CarShop.Console
         /// <param name="args">arguments</param>
         internal static void Main(string[] args)
         {
+            // Initiating Menu
+            MainMenu menu = new MainMenu();
+
+            SubMenu list = new SubMenu("List", "L");
+            list.SubMenuItems.Add(new ListBrand("List Brand", "LB"));
+            list.SubMenuItems.Add(new ListModel("List Model", "LM"));
+            list.SubMenuItems.Add(new ListExtraPerModel("List Extra Per Model", "LEM"));
+            list.SubMenuItems.Add(new ListExtra("List Extra", "LE"));
+            list.SubMenuItems.Add(new ListBrandsModelsPerCountry("List Brands and Models Per Country", "LBM"));
+            menu.SubMenus.Add(list);
+
+            SubMenu create = new SubMenu("Create", "C");
+            create.SubMenuItems.Add(new CreateBrand("Create Brand", "CB"));
+            create.SubMenuItems.Add(new CreateModel("Create Model", "CM"));
+            create.SubMenuItems.Add(new CreateExtra("Create Extra", "CE"));
+            menu.SubMenus.Add(create);
+
+            SubMenu update = new SubMenu("Update", "U");
+            update.SubMenuItems.Add(new UpdateBrand("Update Brand", "UB"));
+            update.SubMenuItems.Add(new UpdateModel("Update Model", "UM"));
+            update.SubMenuItems.Add(new UpdateExtra("Update Extra", "UE"));
+            menu.SubMenus.Add(update);
+
+            SubMenu delete = new SubMenu("Update", "U");
+            delete.SubMenuItems.Add(new DeleteBrand("Delete Brand", "DB"));
+            delete.SubMenuItems.Add(new DeleteModel("Delete Model", "DM"));
+            delete.SubMenuItems.Add(new DeleteExtra("Delete Extra", "DE"));
+            menu.SubMenus.Add(delete);
+
+            menu.StartMenu();
         }
     }
 }
