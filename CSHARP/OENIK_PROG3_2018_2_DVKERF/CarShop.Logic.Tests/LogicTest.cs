5c1aec30b9f29a262c66f862c19996a6f3308518
REPLAY\R3pl4y <nfodor4@gmail.com>
2018-11-23 23:48:19 +0100

    Unit test GetExtra Logic.

--- CSHARP/OENIK_PROG3_2018_2_DVKERF/CarShop.Logic.Tests/LogicTest.cs
+++ CSHARP/OENIK_PROG3_2018_2_DVKERF/CarShop.Logic.Tests/LogicTest.cs
@@ -35,6 +35,14 @@
             this.mockedRepository
                 .Setup(m => m.GetModelsRepo())
                 .Returns(new[] { model1, model2, model3 });
+
+            extra extra1 = new extra() { id = 1, name = "turbo", category_name = "Engine", price = 20 };
+            extra extra2 = new extra() { id = 2, name = "air conditioning", category_name = "Interior", price = 20 };
+            extra extra3 = new extra() { id = 3, name = "dark windows", category_name = "Exterior", price = 20 };
+
+            this.mockedRepository
+                .Setup(m => m.GetExtraRepo())
+                .Returns(new[] { extra1, extra2, extra3 });
         }
 
         [Test]
@@ -64,7 +72,20 @@
             // Assert
             Assert.That(result.Count(), Is.EqualTo(3));
             Assert.That(result.SingleOrDefault(x => x.name == "406"), Is.Not.Null);
+        }
 
+        [Test]
+        public void WhenGettingExtrasFromDb_GetsExtras()
+        {
+            // Mock
+            Logic logic = new Logic(this.mockedRepository.Object);
+
+            // Act
+            var result = logic.GetExtraLogic();
+
+            // Assert
+            Assert.That(result.Count(), Is.EqualTo(3));
+            Assert.That(result.SingleOrDefault(x => x.name == "turbo"), Is.Not.Null);
         }
     }
 }
